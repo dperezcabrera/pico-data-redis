061b2ea0bded7a18a5c4a6a4c1609e2a1f6da1e8
dperezcabrera <dperezcabrera@gmail.com>
2026-07-11 09:05:34 +0200

    Adopt pico-testing 0.2.0 pico_module and drop duplicated conftest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,7 @@
 import fakeredis
 import pytest
-from pico_ioc import DictSource, configuration, init
 
 import pico_data_redis.factory as factory_module
-
-
-@pytest.fixture(autouse=True)
-def isolate_from_installed_plugins(monkeypatch):
-    monkeypatch.setenv("PICO_BOOT_AUTO_PLUGINS", "false")
 
 
 @pytest.fixture(autouse=True)
@@ -25,18 +19,3 @@ def fake_redis(monkeypatch):
         classmethod(lambda cls, url, **kwargs: fakeredis.FakeRedis(server=server)),
     )
     return server
-
-
-@pytest.fixture
-def make_container():
-    created = []
-
-    def _make(*modules, config=None):
-        cfg = configuration(DictSource(config or {}))
-        container = init(modules=["pico_data_redis", *modules], config=cfg)
-        created.append(container)
-        return container
-
-    yield _make
-    for c in reversed(created):
-        c.shutdown()
